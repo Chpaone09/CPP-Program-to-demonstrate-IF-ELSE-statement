0ed6c998fdb415bfe10778bb42e38d2ed7401882
Pawankumar Chidurala <chpaone09@hotmail.com>
2020-10-11 22:17:46 +0530

    Program to demonstrate IF-ELSE Statement in cpp

---
+++ IfElse.cpp
@@ -0,0 +1,22 @@
+/*
+***********************************************************
+
+Topic   : Program to demonstrate IF-ELSE Statement in cpp.
+Author  : Chpaone09®
+Date    : Oct 11, 2020
+
+************************************************************
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 19;
+    if((a%2)==0){
+        cout<<"\n\n\t "<<a<<" is EVEN";
+    }
+    else{
+        cout<<"\n\n\t "<<a<<" if ODD";
+    }
+}
